cb4b61472c94b93f3fe1bd13a9aa23457a097408
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2012-01-07 12:15:20 +0900

    とりあえずセッションを使って簡易ログインもどき

--- web.rb
+++ web.rb
@@ -5,13 +5,33 @@ require './gmail_client'
 
 class Web < Sinatra::Base
   include ERB::Util
+  enable :sessions
+
   get '/' do
+    if ! session["address"] and ! session["password"]
+      redirect '/login'
+    end
+
     client = GmailClient.new
     @mails = client.list_new_mail
     erb :index
   end
 
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    session["address"]  = params[:address]
+    session["password"] = params[:password]
+    redirect '/'
+  end
+
   get '/todo' do
+    session["count"] ||= 0
+    session["count"] += 1
+    puts session["count"]
+
     @todo = [
       'css',
       'おーとぺーじゃー',
